65c03ba77ac4602a57bfa6bed2f8132bbb6ad274
Joalor64 <rhgarcon@gmail.com>
2024-01-16 17:15:08 -0500

    crash handler

--- source/Main.hx
+++ source/Main.hx
@@ -3,12 +3,25 @@ package;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
+#if desktop
+import openfl.events.UncaughtErrorEvent;
+
+import haxe.CallStack;
+import haxe.io.Path;
+
+import sys.io.File;
+import sys.io.Process;
+import sys.FileSystem;
+#end
+
 import display.ToastCore;
 import display.FPS;
 
 #if linux
 import lime.graphics.Image;
 #end
+
+using StringTools;
 
 class Main extends Sprite
 {
@@ -29,6 +42,10 @@ class Main extends Sprite
 		fps = new FPS(10, 3, 0xFFFFFF);
 		addChild(fps);
 
+		#if desktop
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+		#end
+
 		#if linux
 		var icon = Image.fromFile("icon.png");
 		Lib.current.stage.window.setIcon(icon);
@@ -37,4 +54,43 @@ class Main extends Sprite
 		toast = new ToastCore();
 		addChild(toast);
 	}
+
+	#if desktop
+	function onCrash(e:UncaughtErrorEvent):Void
+	{
+		var errMsg:String = "";
+		var path:String;
+		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+		var dateNow:String = Date.now().toString();
+
+		dateNow = dateNow.replace(" ", "_");
+		dateNow = dateNow.replace(":", "'");
+
+		path = "./crash/" + "VisionSphere_" + dateNow + ".txt";
+
+		for (stackItem in callStack)
+		{
+			switch (stackItem)
+			{
+				case FilePos(s, file, line, column):
+					errMsg += file + " (line " + line + ")\n";
+				default:
+					Sys.println(stackItem);
+			}
+		}
+
+		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the GitHub page: https://github.com/Joalor64GH/VisionSphere\n\n> Crash Handler written by: sqirra-rng";
+
+		if (!FileSystem.exists("./crash/"))
+			FileSystem.createDirectory("./crash/");
+
+		File.saveContent(path, errMsg + "\n");
+
+		Sys.println(errMsg);
+		Sys.println("Crash dump saved in " + Path.normalize(path));
+
+		Application.current.window.alert(errMsg, "Error!");
+		Sys.exit(0);
+	}
+	#end
 }
