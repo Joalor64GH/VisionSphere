e401137446ed61b0ed402197c4da2cd01f030e5d
Joalor64 <rhgarcon@gmail.com>
2024-01-15 20:29:02 -0500

    Update MenuState.hx

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -63,25 +63,11 @@ class MenuState extends FlxState
     {
         super.update(elapsed);
 
-        if (FlxG.mouse.overlaps(btnPlay))
-        {
-            FlxG.sound.play(Paths.sound('scroll'));
-            if (FlxG.mouse.pressed) 
-            {
-                FlxG.switchState(new states.PlayState());
-                FlxG.sound.play(Paths.sound('confirm'));
-            }
-        }
+        if (FlxG.mouse.overlaps(btnPlay) && FlxG.mouse.pressed)
+             FlxG.switchState(new states.PlayState());
         #if MODS_ALLOWED
-        else if (FlxG.mouse.overlaps(btnMods))
-        {
-            FlxG.sound.play(Paths.sound('scroll'));
-            if (FlxG.mouse.pressed) 
-            {
-                FlxG.switchState(new states.ModsState());
-                FlxG.sound.play(Paths.sound('confirm'));
-            }
-        }
+        else if (FlxG.mouse.overlaps(btnMods) && FlxG.mouse.pressed)
+            FlxG.switchState(new states.ModsState());
         #end
 
         dateText.text = DateTools.format(Date.now(), "%F") + ' / ' + DateTools.format(Date.now(), "%r");
