15991c9867319599f54ec373e5090a514688e302
Joalor64 <rhgarcon@gmail.com>
2024-01-21 15:53:07 -0500

    whwaou

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -34,8 +34,6 @@ class PlayState extends FlxState
     var titleTxt:FlxText;
     var descTxt:FlxText;
 
-    var games:Array<Game>;
-
     #if (flixel_addons < "3.0.0")
     var checker:FlxBackdrop = new FlxBackdrop(Paths.image('game/grid'), 0.2, 0.2, true, true);
     #else
@@ -53,7 +51,7 @@ class PlayState extends FlxState
         var bars:FlxSprite = new FlxSprite().loadGraphic(Paths.image('game/bars'));
         add(bars);
 
-        games = [
+        var games:Array<Game> = [
             {img: "2048", desc: "2048, but if it was a clicker game!", title: "2048 Clicker"},
             {img: "dvd", desc: "No further explanation needed.", title: "DVD Screensaver"},
             {img: "jta", desc: "The journey ever!", title: "Journey Through Aubekhia"},
@@ -120,7 +118,7 @@ class PlayState extends FlxState
         if (FlxG.keys.justPressed.ENTER)
         {
             FlxG.sound.play(Paths.sound('confirm'));
-            switch(games[currentIndex])
+            switch(currentIndex)
             {
                 case 0:
                     Main.toast.create('Hey!', 0xFFFFFF00, 'This game is not done yet! Stay tuned!');
