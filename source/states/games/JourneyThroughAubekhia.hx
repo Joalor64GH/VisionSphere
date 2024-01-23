3bc89a5444701f8659ad3be5e039326bdb76f064
Joalor64 <rhgarcon@gmail.com>
2024-01-23 12:42:58 -0500

    hmmmm??

--- source/states/games/JourneyThroughAubekhia.hx
+++ source/states/games/JourneyThroughAubekhia.hx
@@ -131,7 +131,7 @@ class PlayState extends FlxState
         if (player.velocity.x != 0)
             player.flipX = player.velocity.x < 0;
 
-        if (jumping && !(Input.is('up') || Input.is('up_alt') || Input.is('accept_alt")))
+        if (jumping && !(Input.is('up') || Input.is('up_alt') || Input.is('accept_alt')))
             jumping = false;
 
         if (player.isTouching(DOWN) && !jumping)
