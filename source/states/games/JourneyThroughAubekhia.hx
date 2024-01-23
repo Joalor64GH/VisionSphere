8960bebeb18d211c4c0a1dbfc44f6e9886edf4af
Joalor64 <rhgarcon@gmail.com>
2024-01-23 11:35:20 -0500

    gonna work on journey through aubekhia next

---
+++ source/states/games/JourneyThroughAubekhia.hx
@@ -0,0 +1,46 @@
+package states.games;
+
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.FlxCamera.FlxCameraFollowStyle;
+import flixel.tile.FlxTilemap;
+import flixel.FlxCamera;
+
+class Vector
+{
+    // wip
+}
+
+class MainMenuState extends FlxState
+{
+    // wip
+}
+
+class PlayState extends FlxState
+{
+    // wip
+}
+
+class LevelCompleteState extends FlxState
+{
+    // wip
+}
+
+class Coin extends FlxSprite
+{
+    // wip
+}
+
+class Flag extends FlxSprite
+{
+    // wip
+}
+
+class Player extends FlxSprite
+{
+    // wip
+}
+
+class Spike extends FlxSprite
+{
+    // wip
+}
