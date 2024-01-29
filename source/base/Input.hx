4f786f9fecbb5b83e55755b08a8b2ba8196b5ffb
Joalor64 <rhgarcon@gmail.com>
2024-01-29 11:29:25 -0500

    69 still funny

--- source/base/Input.hx
+++ source/base/Input.hx
@@ -1,8 +1,6 @@
 package base;
 
 import flixel.input.keyboard.FlxKey;
-import flixel.input.gamepad.FlxGamepad;
-import flixel.input.gamepad.FlxGamepadInputID;
 
 /*
  * @author Leather128
@@ -34,46 +32,6 @@ class Input
             return FlxG.keys.checkStatus(actionMap.get(action), state);
         
         return FlxG.keys.checkStatus(FlxKey.fromString(action), state);
-    }
-
-    public static function get(action:String):flixel.input.FlxInput.FlxInputState
-    {
-        if (is(action, JUST_PRESSED))
-            return JUST_PRESSED;
-        if (is(action, PRESSED))
-            return PRESSED;
-        if (is(action, JUST_RELEASED))
-            return JUST_RELEASED;
-        
-        return RELEASED;
-    }
-}
-
-class GamepadInput
-{
-    public static var gamepadMap:Map<String, FlxGamepadInputID> = [
-        "left" => DPAD_LEFT,
-        "down" => DPAD_DOWN,
-        "up" => DPAD_UP,
-        "right" => DPAD_RIGHT,
-        "accept" => A,
-        "exit" => B
-    ];
-
-    public function is(action:String, ?state:flixel.input.FlxInput.FlxInputState = JUST_PRESSED, ?exact:Bool = false):Bool
-    {
-        if (!exact)
-        {
-            if (state == PRESSED && is(action, JUST_PRESSED))
-                return true;
-            if (state == RELEASED && is(action, JUST_RELEASED))
-                return true;
-        }
-
-        if (gamepadMap.exists(action))
-            return FlxGamepad.checkStatus(gamepadMap.get(action), state);
-
-        return FlxGamepad.checkStatus(FlxGamepad.fromString(action), state);
     }
 
     public static function get(action:String):flixel.input.FlxInput.FlxInputState
