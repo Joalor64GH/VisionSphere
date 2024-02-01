108f0a9e9d58929510be88b7a030c7a3c799db3b
Joalor64 <rhgarcon@gmail.com>
2024-02-01 11:27:17 -0500

    note: fix that later

--- source/states/BootState.hx
+++ source/states/BootState.hx
@@ -1,8 +1,6 @@
 package states;
 
 import util.PlatformUtil;
-import djFlixel.gfx.TextScroller;
-import djFlixel.gfx.pal.Pal_DB32;
 
 class BootState extends FlxState
 {
@@ -15,38 +13,14 @@ class BootState extends FlxState
         FlxG.sound.volumeDownKeys = [NUMPADMINUS];
         FlxG.sound.volumeUpKeys = [NUMPADPLUS];
 
-        var ts:TextScroller = new TextScroller("Loading... Please Wait :)", {
-                f:Paths.font('vcr.ttf'), 
-                s:16, 
-                c:Pal_DB32.COL[21], 
-                bc:Pal_DB32.COL[1]
-            }, {
-                y:100, 
-                speed:2, 
-                loopMode:0,
-                sHeight:32, 
-                w0:4, 
-                w1:0.06
-            }
-        );
-        add(ts);
+        #if desktop
+        states.UpdateState.updateCheck();
+        FlxG.switchState((states.UpdateState.mustUpdate) ? new states.UpdateState() : (FlxG.save.data.firstLaunch) ? new states.FirstLaunchState() : new states.SplashState());
+        #else
+        trace('Sorry! No update support on: ' + PlatformUtil.getPlatform() + '!')
+        FlxG.switchState((FlxG.save.data.firstLaunch) ? new states.FirstLaunchState() : new states.SplashState());
+        #end
 
         super.create();
-    }
-
-    override public function update(elapsed:Float)
-    {
-        new FlxTimer().start(5, function(tmr:FlxTimer)
-        {
-            #if desktop
-            states.UpdateState.updateCheck();
-            FlxG.switchState((states.UpdateState.mustUpdate) ? new states.UpdateState() : (FlxG.save.data.firstLaunch) ? new states.FirstLaunchState() : new states.SplashState());
-            #else
-            trace('Sorry! No update support on: ' + PlatformUtil.getPlatform() + '!')
-            FlxG.switchState((FlxG.save.data.firstLaunch) ? new states.FirstLaunchState() : new states.SplashState());
-            #end
-        });
-
-        super.update(elapsed);
     }
 }
