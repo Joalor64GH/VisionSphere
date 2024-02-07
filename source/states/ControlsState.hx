fbba2a704c857120a0a4c159e33126e6caaa51a9
Joalor64 <rhgarcon@gmail.com>
2024-02-07 10:22:44 -0500

    working i guess

--- source/states/ControlsState.hx
+++ source/states/ControlsState.hx
@@ -16,6 +16,23 @@ class ControlsState extends FlxState
     override public function create()
     {
         super.create();
+
+        var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('theme/' + SaveData.theme));
+        add(bg);
+
+        var instructionsTxt:FlxText = new FlxText(5, FlxG.height - 24, 0, "Press LEFT/RIGHT to change your keys.", 12);
+        instructionsTxt.setFormat(Paths.font('vcr.ttf'), 26, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(instructionsTxt);
+
+        text1 = new FlxText(0, 0, 0, "", 64);
+        text1.screenCenter(Y);
+        text1.x += 100;
+        text1.setFormat(Paths.font('vcr.ttf'), 64, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(text1);
+
+        text2 = new FlxText(0, FlxG.height * 0.9 + 10, FlxG.width, "", 32);
+        text2.setFormat(Paths.font('vcr.ttf'), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        add(text2);
     }
 
     override public function update(elapsed:Float)
